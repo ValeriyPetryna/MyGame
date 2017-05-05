f7e8ad2a3cce1957d3bf102308d8e02040b22162
ValeriyPetryna <valeriypetrina@gmail.com>
2017-05-05 17:41:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+ var gameBoard = document.getElementById('board'), playerNumber = 1, players = ['O','X'], resultValues = [];        
+ function onClick(motion) {      
+      motion.value = players[playerNumber];
+      playerNumber == 1 ? playerNumber-- : playerNumber++;
