4e85ffabc3cb91aa8dc745112e812628e273c515
ValeriyPetryna <valeriypetrina@gmail.com>
2017-05-13 18:00:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
- var gameBoard = document.getElementById('board'), playerNumber = 1, players = ['O','X'], resultValues = [];        
+    var gameBoard = document.getElementById('board'), playerNumber = 1, players = ['O','X'], resultValues = [];
+
  function onClick(motion) {      
       motion.value = players[playerNumber];
       playerNumber == 1 ? playerNumber-- : playerNumber++;
+   motion.disabled = "true";
+ }
+
+function reset() {
+      var i, inputs = gameBoard.getElementsByTagName('input');
+      for (i = 0; i < inputs.length; i++) {
+        inputs[i].disabled = inputs[i].value = '';
+      }
+    }
