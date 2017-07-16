d22ccf43af9d918237940cac76a54ac5cf75735e
ValeriyPetryna <valeriypetrina@gmail.com>
2017-07-16 13:33:16 +0300

    Update SecondScript.js

--- SecondScript.js
+++ SecondScript.js
@@ -1,1 +1,58 @@
-//need add some functions but they are not ready now
+const gameBoard = document.getElementById('board');
+const playerNumber = 1;
+const players = ['O','X']; 
+const resultValues = [];
+const comb = [[0, 1, 2], [0, 4, 8], [2, 4, 6], [2, 5, 8], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7]]; //winning combination
+
+function checkWin(value) //Check if game is over(Win/Loose)
+{ 
+const streak;
+const i;
+const j;
+    for (i=0;i<comb.length;i++) 
+    { 
+    streak=comb[i];
+        for(j=0; j<comb.length;j++)
+        {
+            if(resultValues[streak[j]]!=value)
+                break;
+         }
+         if (j==3) return true;    // streak of X-X-X or O-O-O   
+      }
+    return false;
+}
+
+function reset() { // reset gameBoard
+    let i;
+    let inputs = gameBoard.getElementsByTagName('input');
+      for (i = 0; i < inputs.length; i++) {
+        inputs[i].disabled = inputs[i].value = '';
+      }
+    }
+function Winner() // check who win
+{
+  let i;
+  let inputs = gameBoard.getElementsByTagName('input');     
+      for (i = 0; i < inputs.length; i++)
+      {
+        resultValues[i] = inputs[i].value;// check which value on the game board
+      }
+       
+      for (i = 0; i < players.length; i++) 
+      {
+        if (checkWin(players[i])) 
+        {
+          alert('In this battle winner is: '+ players[i] + '   :3');
+          reset();
+          return;
+        }
+      }
+    }
+
+ function onClick(motion)    // X or O on the board
+{   
+      motion.value = players[playerNumber];
+      playerNumber == 1 ? playerNumber-- : playerNumber++;
+   motion.disabled = "true";
+    Winner();
+ }
