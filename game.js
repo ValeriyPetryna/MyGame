da506f68280d332eaffc3c0663ec8ab4e3c061bb
ValeriyPetryna <valeriypetrina@gmail.com>
2017-07-25 22:23:31 +0300

    Fixed bugs
    Fixed check draw, changed name of js/file, added 'display whose turn' button

--- game.js
+++ game.js
@@ -4,26 +4,23 @@ const players = ['O', 'X']
 let resultValues = []
 const comb = [[0, 1, 2], [0, 4, 8], [2, 4, 6], [2, 5, 8], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7]] // winning combination
 let counter = 0
+document.getElementById('move').value = "Next turn is: X !"
+
 function checkWin (value) { // Check if game is over(Win/Loose)
   let streak, i, j
-  let Draw = true
   for (i = 0; i < comb.length; i++) {
     streak = comb[i]
     for (j = 0; j < comb.length; j++) {
       if (resultValues[streak[j]] !== value)
         break
-        Draw === true
     }
     if (j === 3) {
       return true
-      Draw === false
     }
   }
-  if (counter === 9 && Draw === true) {
-    alert('OMG: It is DRAW!')
-    reset()
-  }
+  return false
 }
+
 function reset () { // reset gameBoard
   let i
   const inputs = gameBoard.getElementsByTagName('input')
@@ -31,12 +28,10 @@ function reset () { // reset gameBoard
     inputs[i].disabled = inputs[i].value = ''
     counter = 0
     playerNumber = 1
+    document.getElementById('move').value = "Next turn is: X !"
   }
 }
-function showMove () {
-  alert(' Next move is : ' + players[playerNumber])
-  resultValues = []
-}
+
 function Winner () { // check who win
   let i
   const inputs = gameBoard.getElementsByTagName('input')
@@ -50,12 +45,16 @@ function Winner () { // check who win
       return
     }
   }
+  if (checkWin() === false && counter === 9) {
+    alert('OMG: It is DRAW!')
+    reset()
+  }
 }
+
 function onClick (motion) {    // X or O on the board
   motion.value = players[playerNumber]
-
   playerNumber === 1 ? playerNumber-- : playerNumber++
-  document.getElementById('moveButton').value = players[playerNumber]
+  document.getElementById('move').value = 'Next turn is:' + players[playerNumber] + '!'
   motion.disabled = 'true'
   counter++
   Winner()
