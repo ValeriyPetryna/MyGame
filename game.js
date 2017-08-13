3e092f39f7166c94d5bac95acf968feca9eca0ef
ValeriyPetryna <valeriypetrina@gmail.com>
2017-08-13 11:11:59 +0300

    game data now in array

--- game.js
+++ game.js
@@ -3,11 +3,14 @@ const players = ['O', 'X']
 let playerNumber = 1
 let gameData = [['-','-','-'],['-','-','-'],['-','-','-']]
 
-function onClick (motion,row,col) { // X or O on the board
-
+function onClick (motion,x,y) {
+  position(players,x,y)
   motion.value = players[playerNumber]
-  gameData[row][col] = players[playerNumber]
-  console.log(gameData)
   playerNumber === 1 ? playerNumber-- : playerNumber++
   motion.disabled = 'true'
+}
+
+function position(players,x,y) {
+  gameData[x][y] = players[playerNumber]
+  console.log(gameData)
 }
