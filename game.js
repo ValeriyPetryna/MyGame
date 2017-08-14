e2e9f8e9043e2e95763a3bfcdd6a0bddc1268556
ValeriyPetryna <valeriypetrina@gmail.com>
2017-08-14 16:24:07 +0300

    added reset function

--- game.js
+++ game.js
@@ -1,16 +1,26 @@
 const players = ['O', 'X']
+const gameBoard = document.getElementById('board')
+const inputs = gameBoard.getElementsByTagName('input')
 
 let playerNumber = 1
-let gameData = [['-','-','-'],['-','-','-'],['-','-','-']]
+let gameData = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
 
-function onClick (motion,x,y) {
-  position(players,x,y)
+function onClick (motion, x, y) {
+  position(players, x, y)
   motion.value = players[playerNumber]
   playerNumber === 1 ? playerNumber-- : playerNumber++
   motion.disabled = 'true'
 }
 
-function position(players,x,y) {
+function position (players, x, y) {
   gameData[x][y] = players[playerNumber]
   console.log(gameData)
+}
+
+function reset () {
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].disabled = inputs[i].value = ''
+  }
+  playerNumber = 1
+  gameData = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
 }
